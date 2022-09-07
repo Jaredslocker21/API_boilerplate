a065c25261bb8ddfe72506620924a1f632a4249c
Jaredslocker21 <jaredslocker21@gmail.com>
2022-09-07 08:52:52 +0000

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,24 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-function processOptions(form){
+function processOptions(form) {
     let optArray = [];
 
-    for (let entry of form.entries()) {
-        if(entry[0] === "options") {
-            optArray.push(entry[1]);
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
         }
     }
-    form.delete("options")
+
+    form.delete("options");
+
     form.append("options", optArray.join());
+
     return form;
 }
 
 async function postForm(e) {
+
     const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
@@ -32,8 +36,8 @@ async function postForm(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data);
-    } else { 
+        displayErrors(data);
+    } else {
         displayException(data);
         throw new Error(data.error);
     }
@@ -50,69 +54,30 @@ async function getStatus(e) {
 
     if (response.ok) {
         displayStatus(data);
-<<<<<<< HEAD
-    } else { 
-        displayException(data);
-=======
     } else {
->>>>>>> refs/remotes/origin/main
+        displayException(data);
         throw new Error(data.error);
     }
 
 }
 
-function displayErrors(data) {
-<<<<<<< HEAD
-
-    let results = "";
-
-    let heading = `JSHint Results for ${data.file}`;
-    if (data.total_errors === 0) {
-        results = `<div class="no_errors">No errors reported!</div>`;
-    } else {
-        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
-        for (let error of data.error_list) {
-            results += `<div>At line <span class="line">${error.line}</span>, `;
-            results += `column <span class="column">${error.col}:</span></div>`;
-            results += `<div class="error">${error.error}</div>`;
-        }
-    }
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
-}
-
-function displayStatus(data) {
-
-    let heading = "API Key Status";
-    let results = `<div>Your key is valid until</div>`;
-    results += `<div class="key-status">${data.expiry}</div>`;
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
-
-}
-
 function displayException(data) {
-    let heading = 'An Exceptiona Occured';
 
-    results = '<div>The API returned a status code ${data.status_code}</div>';
-    results += '<div>Error number: <strong> ${data.error_no}</strong></div>';
-    results += '<div>Error text: <strong> ${data.error}</strong></div>';
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
-=======
->>>>>>> refs/remotes/origin/main
+}
+
+function displayErrors(data) {
 
     let results = "";
 
-<<<<<<< HEAD
-}
-=======
     let heading = `JSHint Results for ${data.file}`;
     if (data.total_errors === 0) {
         results = `<div class="no_errors">No errors reported!</div>`;
@@ -124,7 +89,6 @@ function displayException(data) {
             results += `<div class="error">${error.error}</div>`;
         }
     }
->>>>>>> refs/remotes/origin/main
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
@@ -142,3 +106,4 @@ function displayStatus(data) {
     resultsModal.show();
 
 }
+
